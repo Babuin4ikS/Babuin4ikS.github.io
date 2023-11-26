973f3c4acb473b7220e46b25cb60336a9f587160
Babuin4ikS <sirbunicu333@gmail.com>
2023-11-27 01:29:26 +0200

    Update navbar.js

--- JS/navbar.js
+++ JS/navbar.js
@@ -3,7 +3,7 @@ let prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
   let currentScrollPos = window.pageYOffset;
 
-  if (currentScrollPos === 10 || (currentScrollPos > 10 && currentScrollPos < 100)) {
+  if (currentScrollPos === 0 || (currentScrollPos > 0 && currentScrollPos < 120)) {
     document.getElementById("navbar").classList.remove("scrolled");
   } else {
     document.getElementById("navbar").classList.add("scrolled");
